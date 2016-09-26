fff741fda2c25b15760afb8a2943d57acc04d592
Daniel <truenorthtrainingcompany@gmail.com>
2016-09-26 14:26:26 -0400

    Post Controller TEST pass Should get new

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -4,4 +4,9 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get new" do
+    get '/posts/new'
+    assert_response :success
+  end
 end
