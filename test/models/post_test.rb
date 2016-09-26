81cc9003fe3acdc9a72ac534afb6b10aed8826fb
Daniel <truenorthtrainingcompany@gmail.com>
2016-09-26 13:34:41 -0400

    Replace name with title on Post model test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,11 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should be invalid without a title" do
+    @post.title = nil
+    assert_nil @post.title
+    assert_equal false, @post.valid?
+  end
+
 end
